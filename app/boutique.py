ce5c4abcb443a9e8d9a300fee07989ab1b9a4f41
MathPrm <primotmathilde@gmail.com>
2025-11-02 17:57:27 +0100

    feat(boutique): init variables

--- app/boutique.py
+++ app/boutique.py
@@ -1,1 +1,12 @@
 # this file contains business logic that uses utilities to implement specific rules
+
+# variables
+
+nom_boutique: str = "ChossettZ"
+produit: str = "chaussettes"
+prix_unitaire: float = 5.99
+quantite_stock: int = 20
+tva: float = 0.20
+compte_client: int = 100
+compte_boutique: int = 0
+
