ac71ccf6ff6f3c73c7078bc9119c2004b88b66d7
MathPrm <primotmathilde@gmail.com>
2025-11-04 23:57:19 +0100

    feat(chaussettes): ask user how much socks he desires

--- app/boutique.py
+++ app/boutique.py
@@ -1,7 +1,6 @@
 # this file contains business logic that uses utilities to implement specific rules
 
 # variables
-
 nom_boutique: str = "ChossettZ"
 produit: str = "chaussettes"
 prix_unitaire: float = 5.99
@@ -10,3 +9,14 @@ tva: float = 0.20
 compte_client: int = 100
 compte_boutique: int = 0
 
+# let the user enter how much socks he would like to buy
+def combien_de_chaussettes() -> int:
+    try:
+        chaussettes_nombres = int(input("Veuillez entrer le nombre de chaussettes que vous souhaitez acheter : "))
+    except ValueError:
+        raise Exception("Veuillez rentrer un nombre entier et valide !")
+    if chaussettes_nombres <= 0:
+        raise Exception("Le nombre de chaussettes désirées ne peut être inférieur ou égal à 0 ! x(")
+    if chaussettes_nombres > quantite_stock:
+        raise Exception("La quantité de chaussettes souhaitées dépasse la limite des stocks diponibles, veuillez choisir un stock plus petit, ne vous en déplaise ! :D")
+    return print(f"Vous souhaitez acheter {chaussettes_nombres} chaussetZ ! Excellent choix!")
--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from app.boutique import (
     compte_client,
     compte_boutique,
     quantite_stock,
+    combien_de_chaussettes
 )
 
 def main():
 
     # display a sentence using all variables
     print(f"Chez {nom_boutique}, nous vendons des {produit} au prix imbattable de {prix_unitaire} € et nous appliquons une tva exceptionnelle de {tva}, venez vite rejoindre nos {compte_client} comptes clients auprès de nos {compte_boutique} comptes boutique avant que les {quantite_stock} dernières chaussettes ne disparaissent!")
+    
+    # ask the user to enter the number of socks he would like to buy
+    combien_de_chaussettes()
 
 # entry point
 if __name__ == "__main__":
